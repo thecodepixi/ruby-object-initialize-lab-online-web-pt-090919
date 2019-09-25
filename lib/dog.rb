af19031cef2f0ae9c45d180b841c406022354213
Emily Harber <emily.a.harber@gmail.com>
2019-09-25 01:38:50 +0000

    Done.

---
+++ lib/dog.rb
@@ -0,0 +1,22 @@
+class Dog 
+  def initialize(name, breed="Mutt")
+    @name = name
+    @breed = breed 
+  end 
+  
+  def name=(name)
+    @name = name 
+  end 
+  
+  def name 
+    puts @name 
+  end 
+  
+  def breed=(breed)
+    @breed = breed 
+  end 
+  
+  def breed 
+    puts @breed
+  end 
+end 
